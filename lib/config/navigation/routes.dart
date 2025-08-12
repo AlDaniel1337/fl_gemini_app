e739d233f8bf04f578fe26f94c8977d754d37985
Aldo <al-danmar@hotmail.com>
2025-08-12 12:33:19 -0600

    Agregada navegación inicial

--- lib/config/navigation/routes.dart
+++ lib/config/navigation/routes.dart
@@ -1,15 +1,33 @@
+import 'package:gemini_app/presentation/pages/basic_prompt/basic_prompt.page.dart';
 import 'package:gemini_app/presentation/pages/home/home.page.dart';
 import 'package:get/get.dart';
 
-class Routes{
+enum Routes {
+  home        (url: HomePage.route ),
+  basicPrompt (url: BasicPromptPage.route );
+
+  const Routes({ 
+    required String url 
+  }) : _url = url;
+
+  final String _url;
+  String get url => _url;
+}
+
+
+
+class Navigation{
 
   static const String home = HomePage.route;
   
   static List<GetPage> getRoutes(){    
     List<GetPage> pages = [
       GetPage(name: HomePage.route, page:() => const HomePage()),
+      GetPage(name: BasicPromptPage.route, page:() => const BasicPromptPage()),
     ];
     
     return pages;
   } 
+
+  static goToPage({required Routes page}) => Get.toNamed( page.url );
 }
--- lib/presentation/pages/home/home.page.dart
+++ lib/presentation/pages/home/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gemini_app/config/navigation/routes.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -15,9 +16,19 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
    
-      body: const Center(
-        child: Text('HomePage'),
-      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              backgroundColor: Color.fromARGB(255, 243, 187, 192),
+              child: Icon( Icons.person_outlined ),              
+            ),
+            title: const Text("Prompt básico a Gemini"),
+            subtitle: const Text("Usando un modelo flash"),
+            onTap: () => Navigation.goToPage( page: Routes.basicPrompt ),
+          )
+        ],
+      )
     );
   }
 }
